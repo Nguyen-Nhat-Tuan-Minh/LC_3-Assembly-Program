4933e91969d7915acefcfb9f172e98d6a7b3e439
Minh Nguyen <145042210+Nguyen-Nhat-Tuan-Minh@users.noreply.github.com>
2024-01-02 22:11:51 +0700

    Square with User Input

--- squ.asm
+++ squ.asm
@@ -1,3 +1,16 @@
+; LC-3 Assembly Program: Square with value from User Input
+; Author: Minh Nguyen Nhat Tuan
+; Date: 01/02/2024
+
+; Description:
+; This LC-3 assembly program demonstrates a square operation where the user
+; input the number from 0-9 and the program outputs the results to the monitor.
+
+; How to Use:
+; 1. Run the program on an LC-3 simulator.
+; 2. Input value from 0-9 on keyboard.
+; 3. The results will be displayed on the monitor.
+
 .ORIG x3000
 
 MAIN
